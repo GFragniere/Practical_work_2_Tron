d9635c234f3ec48cd26254146c77d5525bc0b7e0
Killian Viquerat <kil.viquerat@gmail.com>
2024-12-05 15:36:29 +0100

    Fix all connectivity problem

--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -5,7 +5,7 @@ import picocli.CommandLine;
 @CommandLine.Command(
         description = "Tronocol game made with protocol UDP.",
         version = "1.0.0",
-        subcommands = {Server.class, Client.class},
+        subcommands = {Server.class, Client.class, ListNetworkInterfaces.class},
         scope = CommandLine.ScopeType.INHERIT,
         mixinStandardHelpOptions = true)
 public class Root {}
